9cacd8969fb7cacef30a3d1eff1a8324069abaa0
Jacob Hands <jacob@gogit.io>
2021-08-07 18:38:41 +0000

    Add minecraft

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import datetime
 from discord.ext import tasks
 from dblog import dblog
 from replit import db
+from mcstatus import MinecraftServer
+from logzero import logger
 
 
 class MyClient(discord.Client):
@@ -56,12 +58,37 @@ class MyClient(discord.Client):
                 db.set('x:followers_count', followers_count)
 
         except Exception as e:
-            print(e)
+            logger.exception(e)
             dblog(e)
 
     @update_followers.before_loop
     async def before_my_task(self):
         await self.wait_until_ready()  # wait until the bot logs in
+    
+    @tasks.loop(minutes=1)
+    async def update_mc_players(self):
+        try:
+            key = 'x:minecraft:playing_count'
+            server = MinecraftServer.lookup('{0}:25565'.format(os.environ['MC_SERVER']))
+            status = server.status()
+            online = status.players.online
+            if (online != db.get(key, -1)):
+                new_name = 'minecraft-{0}'.format(online)
+                channel = self.get_channel(os.environ['MC_CHANNEL_ID'])
+                await channel.edit(name = new_name)
+                db.set(key, online)
+                logger.info(new_name)
+                dblog(new_name)
+
+        except Exception as e:
+            logger.exception(e)
+            dblog(e)
+    
+    @update_mc_players.before_loop
+    async def before_mc_players(self):
+        await self.wait_until_ready()  # wait until the bot logs in
+
+    
 
 
 client = MyClient()
